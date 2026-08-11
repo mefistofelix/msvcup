44a9e114a97b799933ae767ddd68bed901244e80
Michele <1835312+mefistofelix@users.noreply.github.com>
2026-08-11 15:45:00 +0200

    Build releases through build script

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 # EXTERNAL COMMANDS USED BY THIS BUILD SCRIPT: wget mkdir tar sha256sum
 
 # Determine the absolute directory containing this script using only Bash.
@@ -30,25 +32,13 @@ if [[ ! -e "$go_root" ]]; then
   if [[ ! -e "$script_dir/build/$go_archive" ]]; then
     echo "downloading golang..."
     wget -q "$go_url" -O "$script_dir/build/$go_archive"
-    if [ $? -ne 0 ]; then
-      echo "compiler download failed, aborting."
-      exit 1
-    fi
   fi
 
   echo "$go_checksum  $script_dir/build/$go_archive" | sha256sum -c -
-  if [ $? -ne 0 ]; then
-    echo "compiler checksum verification failed, aborting."
-    exit 1
-  fi
 
   echo "extracting golang..."
   mkdir -p "$go_root"
   tar --strip-components=1 -xf "$script_dir/build/$go_archive" -C "$go_root"
-  if [ $? -ne 0 ]; then
-    echo "compiler extraction failed, aborting."
-    exit 1
-  fi
 fi
 
 echo "building $project_name..."
@@ -58,27 +48,8 @@ go_command="$go_root/bin/go"
 export GOTOOLCHAIN=local
 
 "$go_command" -C "$script_dir/src" test ./...
-if [ $? -ne 0 ]; then
-  echo "tests failed, aborting."
-  exit 1
-fi
-
 "$go_command" -C "$script_dir/src" vet ./...
-if [ $? -ne 0 ]; then
-  echo "vet failed, aborting."
-  exit 1
-fi
-
 CGO_ENABLED=0 GOOS=linux GOARCH=amd64 "$go_command" -C "$script_dir/src" build -trimpath -ldflags="-s -w" -o "$script_dir/bin/msvcup-linux-amd64" .
-if [ $? -ne 0 ]; then
-  echo "linux compilation failed, aborting."
-  exit 1
-fi
-
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 "$go_command" -C "$script_dir/src" build -trimpath -ldflags="-s -w" -o "$script_dir/bin/msvcup-windows-amd64.exe" .
-if [ $? -ne 0 ]; then
-  echo "windows compilation failed, aborting."
-  exit 1
-fi
 
 echo "done"
